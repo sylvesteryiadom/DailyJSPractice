42ef6cec391916676db916821cfbffefe4a91e7c
Sylvester Yiadom <sylvesteryiadom@gmail.com>
2020-06-10 07:28:07 -0500

    numberGuesser update

--- numberGuesser/app.js
+++ numberGuesser/app.js
@@ -8,39 +8,62 @@
 const guessInput = document.getElementById('guess-input'),
     guessBtn = document.getElementById('guess-btn'),
     minNum = document.querySelector('.min-num'),
-    maxNum = document.querySelector('.max-num');
+    maxNum = document.querySelector('.max-num'),
+    message = document.querySelector('.message');
 
-let guessesLeft = 3,
-    randomNum = 5,
+let guessesLeft = 2,
     min = 1,
-    max = 10;
+    max = 10,
+    randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+
 
 minNum.textContent = min;
 maxNum.textContent = max;
 
 // event listener
-guessBtn.addEventListener('click', (e) => {
+guessBtn.addEventListener('click', () => {
     const guess = parseInt(guessInput.value);
     if (isNaN(guess) || guess < minNum || guess > maxNum) {
-        // wrong number
-        setMessage(`Enter a number between ${minNum} and ${maxNum}`, `red`);
-
-    } else if (guess !== randomNum) {
-        // wrong guesss
-        guessesLeft -= 1;
-        setMessage(`${guess} is not correct, you have ${guessesLeft} guesses left`, 'red');
-
+        // unqualified number;
+        setMessage(`Enter a number between ${min} and ${max}`, `red`);
+    } // correct number, you won
+    else if (guess === randomNum) {
+        gameOver(true, `${guess} is correct answer, You win!`);
     } else {
-        // correct number
-        console.log(`Correct answer`);
+        // wrong guesss , reduce guessesleft 
+        guessesLeft -= 1;
+        if (guessesLeft <= 0) {
+            // Game Over
+            gameOver(false, `Game over, the correct answer was ${randomNum}`);
+        } else {
+            // wrong answer - continue;
+            setMessage(`${guess} is not correct, you have ${guessesLeft} guesses left`, `red`);
+        }
     }
-    e.preventDefault();
+    // e.preventDefault();
 });
 
-const setMessage = function (message, color) {
-    const paragraph = document.createElement('p');
-    paragraph.appendChild(document.createTextNode(message));
-    paragraph.style.color = color;
-    const container = document.querySelector('.message');
-    document.getElementById('game').insertBefore(paragraph, container);
+function gameOver(won, msg) {
+    let color;
+    won === true ? color = 'green' : color = 'red';
+    guessInput.disabled = true;
+    guessInput.style.borderColor = color;
+    message.style.color = color;
+    // call set message and pass in the message
+    setMessage(msg);
+    // play again functionality
+    guessBtn.value = 'Play again';
+    guessBtn.className += 'play-again';
 }
+
+const setMessage = function (msg, color) {
+    message.textContent = msg;
+    message.style.color = color;
+}
+
+// Play again event listener
+document.getElementById('game').addEventListener('mousedown', (e) => {
+    if (e.target.className === 'play-again') {
+        window.location.reload();
+    }
+});
