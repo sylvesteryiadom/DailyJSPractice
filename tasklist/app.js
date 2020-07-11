d086a1a8f52e3a136c35a938670aac154c4518d7
Sylvester Yiadom <sylvesteryiadom@gmail.com>
2020-07-10 23:41:41 -0500

    refactoring in progress

---
+++ tasklist/app.js
@@ -0,0 +1,37 @@
+// Declare UI variables
+taskInput = document.querySelector('#task');
+form = document.querySelector('#task-form');
+taskList = document.querySelector('.collection');
+filter = document.querySelector('#filter');
+clearBtn = document.querySelector('.clear-tasks');
+
+// load event listener
+loadAlleventListeners();
+
+// event listener function
+function loadAlleventListeners() {
+    // add task event
+    form.addEventListener('submit', addTask);
+};
+
+function addTask(e) {
+    if (taskInput.value === '') {
+        alert(`Enter a task`);
+    }
+    // Create task from scratch
+    const li = document.createElement('li');
+    // add classname
+    li.className = `collection-item`;
+    // add incoming text to li
+    li.appendChild(document.createTextNode(taskInput.value));
+    // create link
+    const link = document.createElement('a');
+    link.className = `delete-item secondary-content`;
+    // add icon HTML
+    link.innerHTML = `<i class="fa fa-remove"></i>`;
+    // append link to li
+    li.appendChild(link);
+    console.log(li);
+
+    e.preventDefault();
+};
