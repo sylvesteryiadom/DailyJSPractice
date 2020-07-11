8f046d214fd7c49a9179a3ede0d1fc1472db1cf4
Sylvester Yiadom <sylvesteryiadom@gmail.com>
2020-07-11 15:17:52 -0500

    added list item

--- tasklist/app.js
+++ tasklist/app.js
@@ -11,12 +11,32 @@ startallEventListeners();
 // write function for list initializing list item
 function startallEventListeners() {
     // add task event
-    form.addEventListener('submit', addTaskitem);
+    form.addEventListener('submit', addTaskItem);
 }
 
 // function for adding task item
-function addTaskitem(e) {
-    console.log(e.target);
+function addTaskItem(e) {
+    if (taskInput.value === '') {
+        alert(`Please enter a task`);
+    } else { // dynamically insert a task item from scratch
+        // create list item
+        const listEl = document.createElement('li');
+        // Add text Node
+        listEl.appendChild(document.createTextNode(taskInput.value));
+        // add a class name to the list item
+        listEl.className = `collection-item`;
+        // add the 'x' mark to it, which is a link/a tag el
+        const linkEl = document.createElement('a');
+        linkEl.className = `delete-item secondary-content`;
+        // add innerHTML remove icon
+        linkEl.innerHTML = `<i class="fa fa-remove"></i>`;
+        // append link element to list item/el
+        listEl.appendChild(linkEl);
+        // add list item to the DOM
+        taskList.appendChild(listEl);
+        // clear task input
+        taskInput.value = '';
+    }
     //prevent form default behaviour;
     e.preventDefault();
 }
